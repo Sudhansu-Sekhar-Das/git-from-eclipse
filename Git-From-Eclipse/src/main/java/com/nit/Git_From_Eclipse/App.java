21a23a4ec2c3c18b13a08332384b38115bbee1af
Sudhansu-Sekhar-Das <45896756+Sudhansu-Sekhar-Das@users.noreply.github.com>
2019-05-28 19:08:44 +0530

    john changes

--- Git-From-Eclipse/src/main/java/com/nit/Git_From_Eclipse/App.java
+++ Git-From-Eclipse/src/main/java/com/nit/Git_From_Eclipse/App.java
@@ -9,6 +9,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println("sanat changes something"+"chiku");
-        System.out.println( "Hello World!"+"first changes from my git" );
+        
     }
 }
